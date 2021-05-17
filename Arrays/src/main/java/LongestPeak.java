ad3f26b9acc941695358d2c11862d378cd53100f
An Nguyen <nguyen.an.1196@gmail.com>
2021-05-17 08:14:20 -0700

    Solve LongestPeak with better complexity; no use of extra collection

--- Arrays/src/main/java/LongestPeak.java
+++ Arrays/src/main/java/LongestPeak.java
@@ -1,42 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class LongestPeak {
 
   public static int longestPeak(int[] array) {
-    List<Integer> tempArr = new ArrayList<>();
-    boolean tipDetected = false;
-    int longestPeak = 0;
-    for (int i = 0; i < array.length - 1; i++) {
-      tempArr.add(array[i]);
-      int currDifference = array[i + 1] - array[i];
-      if (currDifference == 0) {
-        longestPeak = updateLongestPeak(longestPeak, tempArr);
-        tempArr = new ArrayList<>();
-      } else if (currDifference > 0) {
-        if (tipDetected) {
-          longestPeak = updateLongestPeak(longestPeak, tempArr);
-          tempArr = new ArrayList<>();
-          tempArr.add(array[i]);
-        }
-      } else if (currDifference < 0) {
-        if (!tipDetected) {
-          tipDetected = true;
-        } else {
-          longestPeak = updateLongestPeak(longestPeak, tempArr);
-        }
+    int longestPeakLength = 0;
+    int i = 1;
+    while (i < array.length - 1) {
+      boolean tipDetected = array[i - 1] < array[i] && array[i] > array[i + 1];
+      if (!tipDetected) {
+        i++;
+        continue;
       }
+
+      int leftIdx = i - 2;
+      while (leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1]) {
+        leftIdx--;
+      }
+
+      int rightIdx = i + 2;
+      while (rightIdx < array.length && array[rightIdx] < array[rightIdx - 1]) {
+        rightIdx++;
+      }
+
+      longestPeakLength = updateLongestPeak(longestPeakLength, leftIdx, rightIdx);
+      i = rightIdx;
+
     }
-    return longestPeak;
+    return longestPeakLength;
   }
 
-  private static int updateLongestPeak(int longestPeak, List<Integer> array) {
-    return Math.max(array.size(), longestPeak);
+
+  private static int updateLongestPeak(int longestPeakLength, int leftIdx, int rightIdx) {
+    int currPeakLength = rightIdx - leftIdx - 1;
+    return Math.max(currPeakLength, longestPeakLength);
   }
 
   public static void main(String[] args) {
     int[] array = new int[]{1, 3, 2};
-    longestPeak(array);
+    System.out.println(longestPeak(array));
   }
 
-  }
+}
