7667d4be3f6f822a931fe325f8cb47bab8b6d8e0
An Nguyen <nguyen.an.1196@gmail.com>
2021-05-31 16:00:28 -0700

    Solve LongestPalindromicSubstring, need to review and redo

--- Strings/src/main/java/LongestPalindromicSubstring.java
+++ Strings/src/main/java/LongestPalindromicSubstring.java
@@ -1,32 +1,34 @@
+/*
+  Write a function that, given a string, returns its longest palindromic substring.
+
+  A palindrome is defined as a string that's written the same forward and backward. Note that
+  single-character strings are palindromes.
+
+  You can assume that there will only be one longest palindromic substring.
+ */
 public class LongestPalindromicSubstring {
 
+  // O(n^2) time | O(n) space
   public static String longestPalindromicSubstring(String str) {
-    String longestPalindrome = null;
-    int leftIdx = 0;
-    int rightIdx = str.length() - 1;
-    while (leftIdx < rightIdx) {
-      while (str.charAt(leftIdx) != str.charAt(rightIdx) && leftIdx != rightIdx) {
-        rightIdx--;
-      }
-      String possiblePalindrome = str.substring(leftIdx, rightIdx);
-      if (isPalindrome(possiblePalindrome) && possiblePalindrome.length() > longestPalindrome.length()) {
-        longestPalindrome = possiblePalindrome;
-      }
-      leftIdx = rightIdx++;
+    int[] currLongest = new int[]{0, 1};
+    for (int i = 1; i < str.length(); i++) {
+      int[] odd = getLongestPalindrome(i - 1, i + 1, str);
+      int[] even = getLongestPalindrome(i - 1, i, str);
+      int[] longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+      currLongest = currLongest[1] - currLongest[0] > longest[1] - longest[0] ? currLongest : longest;
     }
-
-    return longestPalindrome;
+    return str.substring(currLongest[0], currLongest[1]);
   }
 
-  private static boolean isPalindrome(String str) { // from PalindromeCheck
-    int leftIdx = 0;
-    int rightIdx= str.length();
-    while (leftIdx <= rightIdx) {
-      if (str.charAt(leftIdx) != str.charAt(rightIdx)) return false;
-      leftIdx++;
-      rightIdx--;
+  private static int[] getLongestPalindrome(int leftIdx, int rightIdx, String str) {
+    while (leftIdx >= 0 && rightIdx < str.length()) {
+      if (str.charAt(leftIdx) != str.charAt(rightIdx)) {
+        break;
+      }
+      leftIdx--;
+      rightIdx++;
     }
-    return true;
+    return new int[] {leftIdx + 1, rightIdx};
   }
 
 }
--- week4_05.31-06.06.21.md
+++ week4_05.31-06.06.21.md
@@ -14,6 +14,46 @@
 
 ---
 
+# Strings
+
+## [Longest Palindromic Substring](Strings/src/main/java/LongestPalindromicSubstring.java)
+
+#### Level: Medium 📘
+
+> Write a function that, given a string, returns its longest palindromic substring.
+>
+> A palindrome is defined as a string that's written the same forward and backward. Note that single-character strings are palindromes.
+>
+> You can assume that there will only be one longest palindromic substring.
+
+```java
+public static String longestPalindromicSubstring(String str) {
+  int[] currLongest = new int[]{0, 1};
+  for (int i = 1; i < str.length(); i++) {
+    int[] odd = getLongestPalindrome(i - 1, i + 1, str);
+    int[] even = getLongestPalindrome(i - 1, i, str);
+    int[] longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+    currLongest = currLongest[1] - currLongest[0] > longest[1] - longest[0] ? currLongest : longest;
+  }
+  return str.substring(currLongest[0], currLongest[1]);
+}
+
+private static int[] getLongestPalindrome(int leftIdx, int rightIdx, String str) {
+  while (leftIdx >= 0 && rightIdx < str.length()) {
+    if (str.charAt(leftIdx) != str.charAt(rightIdx)) {
+      break;
+    }
+    leftIdx--;
+    rightIdx++;
+  }
+  return new int[] {leftIdx + 1, rightIdx};
+}
+```
+
+### O(n^2) time | O(n) space
+
+---
+
 # Sorting
 
 ## [Three Numbers Sort](Sortings/src/main/java/ThreeNumSort.java)
