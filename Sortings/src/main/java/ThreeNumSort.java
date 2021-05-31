e97915ad52bda9ed0a760fbbf65207adab830bbe
An Nguyen <nguyen.an.1196@gmail.com>
2021-05-31 08:57:24 -0700

    Update README for all weeks

--- Sortings/src/main/java/ThreeNumSort.java
+++ Sortings/src/main/java/ThreeNumSort.java
@@ -1,3 +1,18 @@
+/*
+  You're given an array of integers and another array of three distinct integers. The first array
+  is guaranteed to only contain integers that are in the second array, and the second array
+  represents a desired order for the integers in the first array. For example, a second array of
+  [x, y, z] represents a desired order of [x, x, ..., x, y, y, ..., y, z, z, ..., z] in the first
+  array.
+
+  Write a function that sorts the first array according to the desired order in the second array.
+  The function should perform this in place (i.e., it should mutate the input array), and it
+  shouldn't use any auxiliary space (i.e., it should run with constant space: O(1) space).
+
+  Note that the desired order won't necessarily be ascending or descending and that the first array
+  won't necessarily contain all three integers found in the second array—it might only contain one
+  or two.
+ */
 public class ThreeNumSort {
 
   public int[] threeNumberSort(int[] array, int[] order) {
--- week1_05.10-05.16.21.md
+++ week1_05.10-05.16.21.md
@@ -1,6 +1,6 @@
 # Algorithm practice
 
-* Documentations and extra notes are in each file.
+* Java documentations and extra notes are in each file.
 * Solutions displayed here are preferably the most optimal
     * Alternative (less optimal) solutions might also be available
 
