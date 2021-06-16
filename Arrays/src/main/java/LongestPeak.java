ecd73d2f5795c4a0d083bc76ee78a00289f4e476
An Nguyen <nguyen.an.1196@gmail.com>
2021-06-15 20:20:28 -0700

    Initialize week6

--- Arrays/src/main/java/LongestPeak.java
+++ Arrays/src/main/java/LongestPeak.java
@@ -1,21 +1,25 @@
+/*
+  Write a function that takes in an array of integers and returns the length of the longest peak in
+  the array.
+
+  A peak is defined as adjacent integers in the array that are strictly increasing until they reach
+  a tip (the highest value in the peak), at which point they become strictly decreasing. At least
+  three integers are required to form a peak.
+
+  For example, the integers [1, 4, 10, 2] form a peak, but the integers [4, 0, 10] don't and neither
+  do the integers [1, 2, 2, 0]. Similarly, the integers [1, 2, 3]  don't form a peak because there
+  aren't any strictly decreasing integers after the 3.
+ */
 public class LongestPeak {
-
-  /*
-  Write a function that takes in an array of integers and returns the length of
-  the longest peak in the array.
-
-  A peak is defined as adjacent integers in the array that are strictly
-  increasing until they reach a tip (the highest value in the peak), at which
-  point they become strictly decreasing. At least three integers are required to
-  form a peak.
-
-  For example, the integers [1, 4, 10, 2] form a peak, but the
-  integers [4, 0, 10] don't and neither do the integers [1, 2, 2, 0]. Similarly, the integers
-  [1, 2, 3]  don't form a peak because there aren't any strictly decreasing integers after the 3.
-   */
 
   // O(n) time | O(1) space. We traverse thru each number once, at most 2 - 3 times (when peak found
   // and traverse to the left) -> still O(n)
+
+  /**
+   * Finds
+   * @param array
+   * @return
+   */
   public static int longestPeak(int[] array) {
     int longestPeakLength = 0;
     int i = 1;
---
+++ week6_06.14-06.20.21.md
@@ -0,0 +1,8 @@
+# Algorithm practice
+
+* Java documentations and extra notes are in each file.
+* Solutions displayed here are preferably the most optimal
+    * Alternative (less optimal) solutions might also be available within each 
+    file
+
+# Week 5: 06/14 - 06/20/2021
