856212f05b4b44da43d949e6e0ec466013e5942b
An Nguyen <nguyen.an.1196@gmail.com>
2021-07-04 20:11:10 -0700

    Finish BST Construction

--- Binary Search Trees/src/main/java/BSTConstruction.java
+++ Binary Search Trees/src/main/java/BSTConstruction.java
@@ -1,6 +1,26 @@
+/*
+  Write a `BST` class for a Binary Search Tree. The class should support:
+  + Inserting values with the `insert` method.
+  + Removing values with the `remove` method; this method should only remove the first instance of
+  a given value.
+  + Searching for values with the `contains` method.
+
+  Note that you can't remove values from a single-node tree. In other words, calling the `remove`
+  method on a single-node tree should simply not do anything.
+
+  Each `BST` node has an integer `value`, a `left` child node, and a `right` child node. A node is
+  said to be a valid `BST` node if and only if it satisfies the BST property: its `value` is
+  strictly greater than the values of every node to its left; its `value` is less than or equal to
+  the values of every node to its right; and its children nodes are either valid `BST` nodes
+  themselves or `None` / `null`.
+*/
 public class BSTConstruction {
 
+  // Average: O(log n) time | O(log n) space
+  // Worst: O(n) time | O(n) space
+  // Assumption: this BST allows duplications and duplications goes to right
   static class BST {
+
     public int value;
     public BST left;
     public BST right;
@@ -10,56 +30,57 @@ public class BSTConstruction {
     }
 
     public BST insert(int value) {
-      BST current = this;
-      while (true) {
-        if (value < current.value) {
-          if (current.left != null) {
-            current = current.left;
-          } else {
-            current.left = new BST(value);
-            break;
-          }
+      if (value < this.value) {
+        if (this.left == null) {
+          BST newNode = new BST(value);
+          this.left = newNode;
         } else {
-          if (current.right != null) {
-            current = current.right;
-          } else {
-            current.right = new BST(value);
-            break;
-          }
+          this.left = this.left.insert(value);
+        }
+      } else { // value >= this.value
+        if (this.right == null) {
+          BST newNode = new BST(value);
+          this.right = newNode;
+        } else {
+          this.right = this.right.insert(value);
         }
       }
       return this;
     }
 
     public boolean contains(int value) {
-      BST current = this;
-      while (current != null) {
-        if (value < current.value) {
-          current = current.left;
-        } else if (value > current.value) {
-          current = current.right;
-        } else {
-          return true;
+      if (value < this.value) {
+        if (this.left == null) {
+          return false;
         }
+        return this.left.contains(value);
+      } else if (value > this.value) {
+        if (this.right == null) {
+          return false;
+        }
+        return this.right.contains(value);
+      } else {
+        return true;
       }
-      return false;
     }
 
     public BST remove(int value) {
-      removeHelper(this, value);
+      this.removeHelper(this, value);
       return this;
     }
 
-    public BST removeHelper(BST root, int value) {
+    private BST removeHelper(BST root, int value) {
       // base case
-      if (root == null) return root;
-      // searching for the node to delete
+      if (root == null) {
+        return root;
+      }
+
+      // searching
       if (value < root.value) {
         root.left = removeHelper(root.left, value);
       } else if (value > root.value) {
         root.right = removeHelper(root.right, value);
-      } else {
-        // node with no child or 1 child
+      } else { // found a match
         if (root.left == null && root.right == null) {
           return null;
         }
@@ -71,23 +92,22 @@ public class BSTConstruction {
           root.value = root.left.value;
           root.right = root.left.right;
           root.left = root.left.left;
-        } else {
-          // root with 2 children
-          root.value = minValue(root.right);
+        } else { // node has both children
+          root.value = findMin(root.right);
           root.right = removeHelper(root.right, root.value);
         }
       }
       return root;
     }
 
-    public int minValue(BST root) {
-      int minValue = root.value;
-      while (root.left != null) {
-        minValue = root.left.value;
-        root = root.left;
+    private int findMin(BST root) {
+      BST currNode = root;
+      while (currNode.left != null) {
+        currNode = currNode.left;
       }
-      return minValue;
+      return currNode.value;
     }
+
   }
 
 }
--- week8_06.28-07.04.21.md
+++ week8_06.28-07.04.21.md
@@ -81,9 +81,112 @@ public static int findClosestValue(BST node, int target, int closest) {
 
 #### Level: Medium 📘
 
-```java
+> Write a `BST` class for a Binary Search Tree. The class should support:
+>  + Inserting values with the `insert` method.
+>  + Removing values with the `remove` method; this method should only remove the first instance of a given value.
+>  + Searching for values with the `contains` method.
+>
+> Note that you can't remove values from a single-node tree. In other words, calling the `remove` method on a single-node tree should simply not do anything.
+>
+> Each `BST` node has an integer `value`, a `left` child node, and a `right` child node. A node is said to be a valid `BST` node if and only if it satisfies the BST property: its `value` is strictly greater than the values of every node to its left; its `value` is less than or equal to the values of every node to its right; and its children nodes are either valid `BST` nodes themselves or `None` / `null`.
 
+```java
+// Assumption: this BST allows duplications and duplications goes to right
+static class BST {
+
+  public int value;
+  public BST left;
+  public BST right;
+
+  public BST(int value) {
+    this.value = value;
+  }
+
+  public BST insert(int value) {
+    if (value < this.value) {
+      if (this.left == null) {
+        BST newNode = new BST(value);
+        this.left = newNode;
+      } else {
+        this.left = this.left.insert(value);
+      }
+    } else { // value >= this.value
+      if (this.right == null) {
+        BST newNode = new BST(value);
+        this.right = newNode;
+      } else {
+        this.right = this.right.insert(value);
+      }
+    }
+    return this;
+  }
+
+  public boolean contains(int value) {
+    if (value < this.value) {
+      if (this.left == null) {
+        return false;
+      }
+      return this.left.contains(value);
+    } else if (value > this.value) {
+      if (this.right == null) {
+        return false;
+      }
+      return this.right.contains(value);
+    } else {
+      return true;
+    }
+  }
+
+  public BST remove(int value) {
+    this.removeHelper(this, value);
+    return this;
+  }
+
+  private BST removeHelper(BST root, int value) {
+    // base case
+    if (root == null) {
+      return root;
+    }
+
+    // searching
+    if (value < root.value) {
+      root.left = removeHelper(root.left, value);
+    } else if (value > root.value) {
+      root.right = removeHelper(root.right, value);
+    } else { // found a match
+      if (root.left == null && root.right == null) {
+        return null;
+      }
+      if (root.left == null) {
+        root.value = root.right.value;
+        root.left = root.right.left;
+        root.right = root.right.right;
+      } else if (root.right == null) {
+        root.value = root.left.value;
+        root.right = root.left.right;
+        root.left = root.left.left;
+      } else { // node has both children
+        root.value = findMin(root.right);
+        root.right = removeHelper(root.right, root.value);
+      }
+    }
+    return root;
+  }
+
+  private int findMin(BST root) {
+    BST currNode = root;
+    while (currNode.left != null) {
+      currNode = currNode.left;
+    }
+    return currNode.value;
+  }
+
+}
 ```
+
+### Average: O(log n) time | O(log n) space. 
+* Worst: O(n) time | O(n) space when tree has 1 branch.
+* Same idea as above problem to make it O(1) space.
 
 ---
 
