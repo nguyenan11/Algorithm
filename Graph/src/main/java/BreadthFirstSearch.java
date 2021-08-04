514ff422b4c5ebab1c840da0bb144d33402921c9
An Nguyen <nguyen.an.1196@gmail.com>
2021-08-04 10:51:00 -0700

    Solve Graph BFS

---
+++ Graph/src/main/java/BreadthFirstSearch.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+  You're given a `Node` class that has a `name` and an array of optional `children` nodes. When put
+  together, nodes form an acyclic tree-like structure.
+
+  Implement the `breadthFirstSearch` method on the `Node` class, which takes in an empty array,
+  traverses the tree using the Breadth-first Search approach (specifically navigating the tree from
+  left to right), stores all of the nodes' names in the input array, and returns it.
+*/
+public class BreadthFirstSearch {
+
+  static class Node {
+
+    String name;
+    List<Node> children = new ArrayList<Node>();
+
+    public Node(String name) {
+      this.name = name;
+    }
+
+    public Node addChild(String name) {
+      Node child = new Node(name);
+      children.add(child);
+      return this;
+    }
+
+    // O(v + e) time | O(v) space - v is number of vertices, e is number of edges
+    public List<String> breadthFirstSearch(List<String> array) {
+      Queue<Node> queue = new LinkedList<>();
+      queue.add(this);
+      while (!queue.isEmpty()) {
+        Node currNode = queue.poll(); // dequeue
+        array.add(currNode.name);
+        queue.addAll(currNode.children);
+      }
+      return array;
+    }
+
+  }
+
+}
