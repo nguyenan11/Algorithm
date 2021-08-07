788f0db2086e588a58d9827bcaa7e69a4c9f8fe4
An Nguyen <nguyen.an.1196@gmail.com>
2021-08-07 12:53:48 -0700

    Finish SingleCycleCheck

--- Graph/src/main/java/SingleCycleCheck.java
+++ Graph/src/main/java/SingleCycleCheck.java
@@ -1,31 +1,29 @@
 public class SingleCycleCheck {
 
+  // O(n) time | O(1) space
   public static boolean hasSingleCycle(int[] array) {
     int counter = 0;
     int idx = 0;
     while (counter < array.length) {
+      if (counter > 0 && idx == 0) {
+        return false;
+      }
       int numMoves = array[idx];
-      if (numMoves == 0) return false;
-      idx = validateMove(numMoves, idx, array.length - 1);
+      idx = validateMove(numMoves, idx, array.length);
       counter++;
     }
     return idx == 0;
   }
 
   public static int validateMove(int numMoves, int idx, int arrayLength) {
-    int newIdx = idx + numMoves;
-    if (newIdx > arrayLength) {
-      newIdx = (newIdx % arrayLength) - 1;
-    }
-    if (newIdx < 0) {
-      newIdx = arrayLength + 1 + newIdx;
-    }
-    return newIdx;
+    int newIdx = (idx + numMoves) % arrayLength;
+    return newIdx >= 0 ? newIdx : newIdx + arrayLength;
   }
 
   public static void main(String args[]) {
     int[] arr = new int[]{2, 3, 1, -4, -4, 2};
-    int[] secondArr= new int[]{2, 2, 2};
+    int[] secondArr = new int[]{2, 2, 2};
+    int[] thirdArr = new int[]{0, 1, 1, 1, 1, 1};
     int[] edgeCase = new int[]{10, 11, -6, -23, -2, 3, 88, 908, -26};
     boolean result = hasSingleCycle(arr);
     System.out.println(result);
