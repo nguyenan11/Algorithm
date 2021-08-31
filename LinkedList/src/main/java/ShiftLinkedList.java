217395407c3884685b4aa3841de196de2da1bb75
nguyenan11 <nguyen.an.1196@gmail.com>
2021-08-30 20:04:09 -0700

    Bruteforce approach for positive and negative k - ShiftLL problem

--- LinkedList/src/main/java/ShiftLinkedList.java
+++ LinkedList/src/main/java/ShiftLinkedList.java
@@ -2,6 +2,29 @@ public class ShiftLinkedList {
 
   public static LinkedList shiftLinkedList(LinkedList head, int k) {
     if (k == 0) return head;
+    if (k > 0) return positiveShift(head, k);
+    return negativeShift(head, k);
+  }
+
+  public static LinkedList negativeShift(LinkedList head, int k) {
+    LinkedList PtrOne = head;
+    int newK = Math.abs(k);
+    for (int i = 1; i < newK; i++) {
+      if (PtrOne == null) PtrOne = head;
+      PtrOne = PtrOne.next;
+    }
+    LinkedList newHead = PtrOne.next;
+    if (newHead == null) return head;
+    LinkedList currNode = PtrOne;
+    while (currNode.next != null) {
+      currNode = currNode.next;
+    }
+    currNode.next = head;
+    PtrOne.next = null;
+    return newHead;
+  }
+
+  public static LinkedList positiveShift(LinkedList head, int k) {
     LinkedList PtrOne = head;
     for (int i = 1; i <= k; i++) {
       if (PtrOne == null) PtrOne = head;
@@ -17,8 +40,7 @@ public class ShiftLinkedList {
     LinkedList newHead = PtrTwo.next;
     PtrTwo.next = null;
     PtrOne.next = head;
-    head = newHead;
-    return head;
+    return newHead;
   }
 
   static class LinkedList {
