d29dfc04645f7a75091091a43e82f97794c6d1f7
nguyenan11 <nguyen.an.1196@gmail.com>
2021-08-31 13:27:13 -0700

    Improve on ShiftLL

--- LinkedList/src/main/java/ShiftLinkedList.java
+++ LinkedList/src/main/java/ShiftLinkedList.java
@@ -1,4 +1,46 @@
+/*
+  Write a function that takes in the head of a Singly Linked List and an integer `k`, shifts the
+  list in place (i.e., doesn't create a brand new list) by k positions, and returns its new head.
+
+  Shifting a Linked List means moving its nodes forward or backward and wrapping them around the
+  list where appropriate. For example, shifting a Linked List forward by one position would make
+  its tail become the new head of the linked list.
+
+  Whether nodes are moved forward or backward is determined by whether `k` is positive or negative.
+
+  Each `LinkedList` node has an integer `value` as well as a `next` node pointing to the next node
+  in the list or to `None` / `null` if it's the tail of the list.
+
+  You can assume that the input Linked List will always have at least one node; in other words, the
+  head will never be `None` / `null`.
+*/
 public class ShiftLinkedList {
+
+  // O(n) time | O(1) space
+  public static LinkedList shiftLinkedList(LinkedList head, int k) {
+    int length = 1;
+    LinkedList tail = head;
+    while (tail.next != null) {
+      tail = tail.next;
+      length++;
+    }
+
+    int offset = Math.abs(k) % length;
+    if (offset == 0) return head;
+    int newTailPosition = k > 0 ? length - offset : offset; // logic for +/-
+    LinkedList newTail = head;
+    for (int i = 1; i < newTailPosition; i++) {
+      newTail = newTail.next;
+    }
+
+    LinkedList newHead = newTail.next;
+    newTail.next = null;
+    tail.next = head;
+    return newHead;
+  }
+
+  /*
+  Brute force: O(k) time | O(1) space
 
   public static LinkedList shiftLinkedList(LinkedList head, int k) {
     if (k == 0) return head;
@@ -42,6 +84,7 @@ public class ShiftLinkedList {
     PtrOne.next = head;
     return newHead;
   }
+  */
 
   static class LinkedList {
 
