fce796043827708b069e3c27614964129e984b31
nguyenan11 <nguyen.an.1196@gmail.com>
2021-09-16 12:55:48 -0700

    Finish LLPalindrome, Stack approach also works

--- LinkedList/src/main/java/LinkedListPalindrome.java
+++ LinkedList/src/main/java/LinkedListPalindrome.java
@@ -1,6 +1,13 @@
 /*
+  Write a function that takes in the head of a Singly Linked List and returns a boolean representing
+  whether the linked list's nodes form a palindrome. Your function shouldn't make use of any
+  auxiliary data structure.
 
- */
+  A palindrome is usually defined as a string that's written the same forward and backward. For a
+  linked list's nodes to form a palindrome, their values must be the same when read from left to
+  right and from right to left. Note that single-character strings are palindromes, which means
+  that single-node linked lists form palindromes.
+*/
 public class LinkedListPalindrome {
 
   // This is an input class. Do not edit.
@@ -15,13 +22,18 @@ public class LinkedListPalindrome {
   }
 
   public boolean linkedListPalindrome(LinkedList head) {
-    LinkedList reversedLLHead = reverseLinkedList(head);
-    LinkedList currNode = head;
-    LinkedList currReverseNode = reversedLLHead;
-    while (currNode != null) {
-      if (currNode.value != currReverseNode.value) return false;
-      currNode = currNode.next;
-      currReverseNode = currReverseNode.next;
+    LinkedList slowNode = head;
+    LinkedList fastNode = head;
+    while (fastNode != null && fastNode.next != null) {
+      slowNode = slowNode.next;
+      fastNode = fastNode.next.next;
+    }
+    LinkedList reversedSecondHalfNode = reverseLinkedList(slowNode);
+    LinkedList firstHalfNode = head;
+    while (reversedSecondHalfNode != null) {
+      if (reversedSecondHalfNode.value != firstHalfNode.value) return false;
+      reversedSecondHalfNode = reversedSecondHalfNode.next;
+      firstHalfNode = firstHalfNode.next;
     }
     return true;
   }
@@ -38,25 +50,40 @@ public class LinkedListPalindrome {
     return prevNode;
   }
 
-  /*
-  This approach will work if the case where odd number of nodes is taken care, which requires more
-  steps
+  /* Alternative | When we cannot modify the LL
+  O(n) time | O(n) space | Implement Recursive using left node, right node for same complexity
+
   public boolean linkedListPalindrome(LinkedList head) {
     Stack<Integer> stack = new Stack<>();
     LinkedList currNode = head;
     while (currNode != null) {
-      if (stack.isEmpty()) {
-        stack.add(currNode.value);
-      } else {
-        if (stack.peek() == currNode.value) {
-          stack.pop();
-        } else {
-          stack.add(currNode.value);
-        }
-      }
+      stack.add(currNode.value);
       currNode = currNode.next;
     }
-    return stack.isEmpty() || stack.size() == 1 ;
+
+		currNode = head;
+		while (currNode != null) {
+			if (stack.peek() != currNode.value) return false;
+			stack.pop();
+			currNode = currNode.next;
+		}
+    return true;
+  }
+  */
+
+
+  /*
+  This approach fails only 1 test case on LL 3 -> 1 -> 2 -> 3
+  public boolean linkedListPalindrome(LinkedList head) {
+    LinkedList reversedLLHead = reverseLinkedList(head);
+    LinkedList currNode = head;
+    LinkedList currReverseNode = reversedLLHead;
+    while (currNode != null) {
+      if (currNode.value != currReverseNode.value) return false;
+      currNode = currNode.next;
+      currReverseNode = currReverseNode.next;
+    }
+    return true;
   }
   */
 
