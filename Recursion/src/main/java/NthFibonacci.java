e373ad7404d6f84f4f480a8d1218bde5ddf09909
An Nguyen <nguyen.an.1196@gmail.com>
2021-12-02 15:25:58 -0800

    Most optimized solution for NthFibonacci

--- Recursion/src/main/java/NthFibonacci.java
+++ Recursion/src/main/java/NthFibonacci.java
@@ -9,6 +9,19 @@
  */
 public class NthFibonacci {
 
+  // O(n) time | O(1) space
+  public static int getNthFib(int n) {
+    int[] lastTwo = {0, 1};
+    int counter = 3;
+    while (counter <= n) {
+      int nextFib = lastTwo[0] + lastTwo[1];
+      lastTwo[0] = lastTwo[1];
+      lastTwo[1] = nextFib;
+      counter++;
+    }
+    return n > 1 ? lastTwo[1] : lastTwo[0]; // handle edge case where n == 1
+  }
+
   /* O(n) time | O(n) space
   public static int getNthFib(int n) {
     Map<Integer, Integer> memory = new HashMap<Integer, Integer>();
