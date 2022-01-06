e03b9d5c4c869c1c149204075f4cf5a2ac514102
An Nguyen <nguyen.an.1196@gmail.com>
2022-01-06 09:58:57 -0800

    Implement Powerset

---
+++ Recursion/src/main/java/Powerset.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+  Write a function that takes in an array of unique integers and returns its powerset.
+
+  The powerset `P(X)` of a set `X` is the set of all subsets of `X`. For example, the powerset of
+  `[1,2]` is `[[], [1], [2], [1,2]]`.
+
+  Note that the sets in the powerset do not need to be in any particular order.
+*/
+public class Powerset {
+
+  // O(n * 2^n) time | O(n * 2^n) space
+  public static List<List<Integer>> powerset(List<Integer> array) {
+    List<List<Integer>> subsets = new ArrayList<List<Integer>>();
+    subsets.add(new ArrayList<Integer>());
+    for (Integer num : array) {
+      int length = subsets.size();
+      for (int i = 0; i < length; i++) {
+        List<Integer> currSubset = new ArrayList<Integer>(subsets.get(i));
+        currSubset.add(num);
+        subsets.add(currSubset);
+      }
+    }
+    return subsets;
+  }
+
+}
