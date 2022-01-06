00f981d753ad2007a32a04aec61057dcf63795ea
An Nguyen <nguyen.an.1196@gmail.com>
2022-01-05 21:12:35 -0800

    Improve complexity for Permutations problem

--- Recursion/src/main/java/Permutations.java
+++ Recursion/src/main/java/Permutations.java
@@ -9,7 +9,32 @@ import java.util.List;
 */
 public class Permutations {
 
-  // O(n^2 * n!) time | O(n * n!) space
+  // O(n * n!) time | O(n * n!) space
+  public static List<List<Integer>> getPermutations(List<Integer> array) {
+    List<List<Integer>> permutations = new ArrayList<List<Integer>>();
+    getPermutations(0, array, permutations);
+    return permutations;
+  }
+
+  public static void getPermutations(int i, List<Integer> array, List<List<Integer>> permutations) {
+    if (i == array.size() - 1) {
+      permutations.add(new ArrayList<Integer>(array));
+    } else {
+      for (int j = i; j < array.size(); j++) {
+        swap(array, i, j);
+        getPermutations(i + 1, array, permutations);
+        swap(array, i, j);
+      }
+    }
+  }
+
+  public static void swap(List<Integer> array, int i, int j) {
+    Integer temp = array.get(i);
+    array.set(i, array.get(j));
+    array.set(j, temp);
+  }
+
+  /* Longer run time sln: O(n^2 * n!) time | O(n * n!) space
   public static List<List<Integer>> getPermutations(List<Integer> array) {
     List<List<Integer>> permutations = new ArrayList<List<Integer>>();
     getPermutations(array, new ArrayList<Integer>(), permutations);
@@ -29,5 +54,6 @@ public class Permutations {
       }
     }
   }
+   */
 
 }
